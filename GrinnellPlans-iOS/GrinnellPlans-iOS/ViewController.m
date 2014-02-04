3464c6ca442e4bdd1c58099fa6201eeb59237044
Daniel <dmaxtorres@gmail.com>
2014-02-03 23:48:16 -0600

    Login Works + gitignore @tremblay

--- GrinnellPlans-iOS/GrinnellPlans-iOS/ViewController.m
+++ GrinnellPlans-iOS/GrinnellPlans-iOS/ViewController.m
@@ -14,56 +14,81 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
--(void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
-{
-    if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust])
-    {
-        if ([challenge.protectionSpace.host isEqualToString:@"http://www.grinnellplans.com/"])
-        {
-            [challenge.sender useCredential:[NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust] forAuthenticationChallenge:challenge];
-        }
-        
-        [challenge.sender continueWithoutCredentialForAuthenticationChallenge:challenge];
-    }
-    else if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodHTTPBasic])
-    {
-        if ([challenge previousFailureCount] == 0)
-        {
-            NSURLCredential *newCredential;
-            
-            newCredential = [NSURLCredential credentialWithUser:self.usernameField.text password:self.passwordField.text persistence:NSURLCredentialPersistenceForSession];
-            
-            [[challenge sender] useCredential:newCredential forAuthenticationChallenge:challenge];
-        }
-        else
-        {
-            [[challenge sender] cancelAuthenticationChallenge:challenge];
-            
-            // inform the user that the user name and password
-            // in the preferences are incorrect
-            
-            NSLog (@"failed authentication");
-            
-            // ...error will be handled by connection didFailWithError
-        }
-    }
+- (void)alertStatus:(NSString *)msg withTitle:(NSString *)title {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title
+                                                        message:msg
+                                                       delegate:self
+                                              cancelButtonTitle:@"Ok"
+                                              otherButtonTitles:nil, nil];
+    [alertView show];
 }
 
 - (IBAction)connect:(id)sender {
-    [self connection:<#(NSURLConnection *)#> didReceiveAuthenticationChallenge:<#(NSURLAuthenticationChallenge *)#>]
+    
+        @try {
+            if ([self.usernameField.text isEqualToString:@""] || [self.passwordField.text isEqualToString:@""]) {
+                [self alertStatus:@"Please enter both Username and Password" withTitle:@"Login Failed!"];
+            } else {
+                NSString *post = [[NSString alloc] initWithFormat:@"username=%@&password=%@&submit=Login&js_test_value=on", self.usernameField.text, self.passwordField.text];
+                //NSLog(@"PostData: %@",post);
+                
+                NSURL *url = [NSURL URLWithString:@"http://grinnellplans.com/index.php"];
+                
+                NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+                
+                NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
+                
+                NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+                [request setURL:url];
+                [request setHTTPMethod:@"POST"];
+                [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+                [request setValue:@"application/html" forHTTPHeaderField:@"Accept"];
+                [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+                [request setHTTPBody:postData];
+                
+                NSError *error = [[NSError alloc] init];
+                NSHTTPURLResponse *response = nil;
+                NSData *urlData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+                
+                //NSLog(@"Response code: %d", [response statusCode]);
+                if ([response statusCode] >= 200 && [response statusCode] < 300) {
+                    NSString *responseData = [[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
+                     //NSLog(@"Response ==> %@", responseData);
+                    
+                    NSRange loginIndicator = [responseData rangeOfString:@"Plans 3"];
+                    BOOL success = (NSNotFound == loginIndicator.location) ? FALSE : TRUE;
+                    
+                    if (success) {
+                        NSLog(@"Login SUCCESS");
+                        // [self alertStatus:@"Logged in Successfully." withTitle:@"Login Success!"];
+                        // TODO - Handle successful login
+                    } else {
+                        //NSString *error_msg = (NSString *) [jsonData objectForKey:@"error_message"];
+                        [self alertStatus:nil withTitle:@"Login Failed!"];
+                    }
+                } else {
+                    if(error) {
+                        NSLog(@"Error: %@", error);
+                    }
+                    [self alertStatus:@"Connection Failed" withTitle:@"Login Failed!"];
+                }
+            }
+        }
+        @catch(NSException * e) {
+            NSLog(@"Exception: %@", e);
+            [self alertStatus:@"Login Failed." withTitle:@"Login Failed!"];
+        }
 }
 
 @end
